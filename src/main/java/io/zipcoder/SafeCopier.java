c4817d0dff914fb3a846b21ea97004ae95f6025b
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-14 16:11:23 -0400

    saving

--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -1,8 +1,31 @@
 package io.zipcoder;
 
+import java.util.NoSuchElementException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
-public class SafeCopier {
+public class SafeCopier extends Copier{
+    public SafeCopier(String toCopy) {
+        super(toCopy);
+    }
+    Lock sharedLock = new ReentrantLock();
+
+    public void run() {
+
+        while(stringIterator.hasNext()){
+            sharedLock.lock();
+            try {
+                //sharedLock.lock();
+                copied += stringIterator.next() + " " + Thread.currentThread().getName();
+            }catch(NoSuchElementException e){}
+            finally{
+                sharedLock.unlock();
+                }
+            }
+
+        }
 }
--- src/test/java/io/zipcoder/SafeCopierTest.java
+++ src/test/java/io/zipcoder/SafeCopierTest.java
@@ -1,9 +1,18 @@
 package io.zipcoder;
 
-public class MonkeyTypewriter {
-    public static void main(String[] args) {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-        String introduction = "It was the best of times,\n" +
+
+
+public class SafeCopierTest {
+
+    String testString;
+
+    @Before
+    public void setup(){
+        testString = "It was the best of times,\n" +
                 "it was the blurst of times,\n" +
                 "it was the age of wisdom,\n" +
                 "it was the age of foolishness,\n" +
@@ -19,28 +28,12 @@ public class MonkeyTypewriter {
                 "we were all going direct the other way--\n" +
                 "in short, the period was so far like the present period, that some of\n" +
                 "its noisiest authorities insisted on its being received, for good or for\n" +
-                "evil, in the superlative degree of comparison only.";
+                "evil, in the superlative degree of comparison only. ";
 
-        // Do all of the Monkey / Thread building here
-        // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
-        // A Tale Of Two Cities.
-
-        UnsafeCopier unsafeCopier = new UnsafeCopier(introduction);
-
-        Thread monkey1= new Thread(unsafeCopier);
-        Thread monkey2= new Thread(unsafeCopier);
-        Thread monkey3= new Thread(unsafeCopier);
-        Thread monkey4= new Thread(unsafeCopier);
-        Thread monkey5= new Thread(unsafeCopier);
-
-        monkey1.start();
-        monkey2.start();
-        monkey3.start();
-        monkey4.start();
-        monkey5.start();
-
-
-        SafeCopier safeCopier = new SafeCopier(introduction);
+    }
+    @Test
+    public void run() {
+        SafeCopier safeCopier = new SafeCopier(testString);
 
         Thread monkeySafe1= new Thread(safeCopier);
         Thread monkeySafe2= new Thread(safeCopier);
@@ -53,18 +46,17 @@ public class MonkeyTypewriter {
         monkeySafe3.start();
         monkeySafe4.start();
         monkeySafe5.start();
-
-
-        // This wait is here because main is still a thread and we want the main method to print the finished copies
-        // after enough time has passed.
         try {
             Thread.sleep(1000);
         } catch(InterruptedException e) {
             System.out.println("MAIN INTERRUPTED");
         }
 
-        // Print out the copied versions here.
-        System.out.println(safeCopier.copied);
+
+        String expected = testString;
+        String actual = safeCopier.copied;
+
+        Assert.assertEquals(expected,actual);
 
     }
 }
