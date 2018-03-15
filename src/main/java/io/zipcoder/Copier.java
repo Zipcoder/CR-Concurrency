7bcf2c9bf1f30e75ace5f9b3f692cf3ba9d7a5ee
vvmk <vincentmasiello@gmail.com>
2018-03-14 21:23:53 -0400

    Copy unsafely

--- src/main/java/io/zipcoder/Copier.java
+++ src/main/java/io/zipcoder/Copier.java
@@ -9,13 +9,17 @@ import java.util.Iterator;
 public abstract class Copier implements Runnable {
     // We use an iterator so each monkey / thread can copy an individual word.
     public Iterator<String> stringIterator;
-    public String copied;
+    protected StringBuffer buffer; // You want unsafe? Try using StringBuilder instead.
 
     public Copier(String toCopy) {
         // Take the input string, split it on spaces, turn that array to an arraylist, and then grab its iterator.
         this.stringIterator = Arrays.asList(toCopy.split(" ")).iterator();
-        this.copied = "";
+        buffer = new StringBuffer();
     }
 
     public abstract void run();
+
+    public String getCopy() {
+        return buffer.toString();
+    }
 }
